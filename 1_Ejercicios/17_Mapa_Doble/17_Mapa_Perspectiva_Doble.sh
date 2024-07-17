de464514fac1f2e6a3c575dee40c2b765ec39641
Esteban82 <federico.esteban@gmail.com>
2024-07-17 11:49:22 -0300

    Agregar mapa doble

--- 1_Ejercicios/17_Mapa_Doble/17_Mapa_Perspectiva_Doble.sh
+++ 1_Ejercicios/17_Mapa_Doble/17_Mapa_Perspectiva_Doble.sh
@@ -5,17 +5,20 @@ clear
 #	-----------------------------------------------------------------------------------------------------------
 #	Titulo del mapa
 	title=17_Mapa_Perspectiva_Doble
+	title=$(basename $0 .sh)
 	echo $title
 
 #	Region Geografica, Proyeccion y perpectiva
 	REGION=-72/-64/-35/-30
 	PROJ=M15c
 	p=160/30
-#	p=180/90
 
-#	Resolucion
+#	Datos
 	RES=01m
+	GRD=@earth_relief_$RES
+	SAT=@earth_day_$RES
 
+	
 	gmt set MAP_FRAME_AXES WesN
 	gmt set GMT_VERBOSE w
 
@@ -33,7 +36,7 @@ gmt begin $title png
 	gmt makecpt -Cdem4 -T0/7000
 
 #	Crear Imagen a partir de grilla con sombreado y diferentes cpt maestros
-	gmt grdimage -p @earth_relief_$RES -I -C
+	gmt grdimage -p $GRD -I -C
 
 #	Agregar escala vertical a partir de CPT (-C). Posición (x,y) +wlargo/ancho. Anotaciones (-Ba). Leyenda (+l). 
 	gmt colorbar -p -DJRM+o0.3c/0+w11/0.618c -C -Ba+l"Elevaciones (km)" -I -W0.001
@@ -49,16 +52,16 @@ gmt begin $title png
 
 #	-----------------------------------------------------------------------------------------------------------
 #	Transicion entre mapas. Subir 2º mapa 7,5 cm.
-	#gmt basemap -B+n -Y3.5c
-	gmt basemap -B+n -Yh+0.3c
-	#gmt basemap -B+n -Xw+0.3c
+	gmt basemap -B+n -Y3.5c     # Subir 3.5 cm
+	#gmt basemap -B+n -Yh+0.3c # Subir la altura de la figura + 0,3 cm
+	#gmt basemap -B+n -Xw+0.3c  # Correr el ancho de la figura + 0,3 cm
 
 #	-----------------------------------------------------------------------------------------------------------
 
 #	Dibujar mapa 2
 #	-----------------------------------------------------------------------------------------------------------
 #	Ubicar Imagen satelital
-	gmt grdimage -p @earth_day_$RES
+	gmt grdimage -p $SAT
 
 #	Dibujar limite paises y provincias
 	gmt coast -p -N1/thinner -N2/thinnest,-
