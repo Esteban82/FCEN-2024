923fa76f382cb13454c2ab5146fe35e7c5d8e381
Esteban82 <federico.esteban@gmail.com>
2024-07-22 01:27:47 -0300

    scrip 28

--- 1_Ejercicios/28_Perfiles_2/28_Perfiles_2.sh
+++ 1_Ejercicios/28_Perfiles_2/28_Perfiles_2.sh
@@ -7,7 +7,8 @@ clear
 #	Define map
 #	-----------------------------------------------------------------------------------------------------------
 #	Titulo del mapa
-	title=46_Perfiles_2
+	title=26_Perfiles_2
+	title=$(basename $0 .sh)
 	echo $title
 
 #	Dimensiones del Grafico: Longitud (L), Altura (H).
@@ -18,11 +19,13 @@ clear
 	RES=01m
 
 #	Base de datos de GRILLAS
-	DEM=@earth_relief_${RES}_p
-	GRD=@earth_faa_01m_p		# Free Air Anomalies
-#	GRD=@earth_vgg_01m_p		# Vertical Gravity Gradient
-#	GRD=@earth_mag4km_02m_p		# EMAG2 a 4 km de altitud
-#	GRD=@earth_geoid_01m_g		# Geoide 
+	GRD0=@earth_relief_${RES}_p
+	GRD1=@earth_faa_01m_p		# Free Air Anomalies
+#	GRD1=@earth_vgg_01m_p		# Vertical Gravity Gradient
+#	GRD1=@earth_mag4km_02m_p	# EMAG2 a 4 km de altitud
+#	GRD1=@earth_geoid_01m_g		# Geoide
+
+	PERFIL=tmp_perfil.txt
 
 #	Dibujar mapa
 #	-----------------------------------------------------------------------------------------------------------
@@ -32,68 +35,61 @@ gmt begin $title png
 #	Calcular Distancia a lo largo de la linea y agregar datos geofisicos
 #	-----------------------------------------------------------------------------------------------------------
 #	Perfil: Crear archivo para dibujar perfil (Long Lat)
-	cat > tmp_line <<- END
+	cat > tmp_line.txt <<- END
 	-76 -32
 	-46 -32
 	END
 
-#	Interpolar: agrega datos en el perfil cada 0.2 km (-I).
-	gmt sample1d tmp_line -I$RES > tmp_sample1d -fg
+#	Interpolar datos (-I)
+	gmt sample1d tmp_line.txt -I$RES > tmp_sample1d.txt -fg
 
 #	Crear variable con region geografica del perfil
-	REGION=$(gmt info tmp_sample1d -I+e0.1)
+	REGION=$(gmt info tmp_sample1d.txt -I+e0.1)
 	echo $REGION
 
 #	Distancia: Agrega columna (3a) con distancia del perfil en km (-G+uk)
-	gmt mapproject tmp_sample1d -G+uk > tmp_track
+	gmt mapproject tmp_sample1d.txt -G+uk > tmp_track.txt
 
 #	Agrega columna (4) con datos extraidos de la grilla -G (altura) sobre el perfil
-#	gmt grdtrack tmp_track -G$DEM $REGION > tmp_data
-	gmt grdtrack tmp_track -G$DEM -G$GRD $REGION > tmp_data
+#	gmt grdtrack tmp_track.txt -G$DEM $REGION >  $PERFIL
+	gmt grdtrack tmp_track.txt -G$GRD0 -G$GRD1 $REGION > $PERFIL
 		
 #	Hacer Grafico y dibujar perfil
 #	-----------------------------------------------------------------------------------------------------------
 #	Informacion para crear el grafico. 3a Columna datos en km. 4a Columna datos de Topografia.
-	gmt info tmp_data
-
-#	Datos para el perfil:
-#	-------------------------------------------------
-#	Distancia del Perfil (km):
-	KM=$(gmt info tmp_data -C -o5)
-
-#	Altura (m) minima y maxima:
-#	Topo_Min=$(gmt info "tmp_data" -C -i2 -o0)
-#	Topo_Max=$(gmt info "tmp_data" -C -i2 -o1)
-#	Grav_Min=$(gmt info "tmp_data" -C -i3 -o0)
-#	Grav_Max=$(gmt info "tmp_data" -C -i3 -o1)
-
-#	Datos del perfil. Min y Max de altura/gravedad
-	Topo=$(gmt info "tmp_data" -C -i3 -o0)/$(gmt info "tmp_data" -C -i3 -o1)
-	Grav=$(gmt info "tmp_data" -C -i4 -o0)/$(gmt info "tmp_data" -C -i4 -o1)
+	gmt info $PERFIL
 
 #	Crear Grafico
-	gmt basemap -JX$L/$H -R0/$KM/$Topo -B+n
+#	-------------------------------------------------
+#	Setear dimensiones del grafico
+	gmt basemap -JX$L/$H -R0/1/0/1 -B+n
+
+#	Dibujar perfil 1
+	COLOR=blue
+	gmt plot -Re $PERFIL -W0.5,$COLOR -i2,3 -l"Topopgrafia"
+	gmt basemap -Byag+l"Elevaciones (m)"              -BW --FONT_ANNOT_PRIMARY=8,Helvetica,$COLOR
+
+#	Dibujar Perfil 2
+	COLOR=red
+	gmt plot -Re $PERFIL -W0.5,$COLOR  -i2,4 -l"Aire Libre"
+	gmt basemap -Bya+l"Anomal\355a Aire Libre (mGal)" -BE --FONT_ANNOT_PRIMARY=8,Helvetica,$COLOR
 
 #	Dibujar Eje X (Sn)
 	gmt basemap -Bxaf+l"Distancia (km)" -BSn
-
-#	Dibujar Eje Y y datos de columnas 3a y 4a (-i2,3)
-	gmt plot -R0/$KM/$Topo tmp_data -W0.5,blue -i2,3 
-	gmt plot -R0/$KM/$Grav tmp_data -W0.5,red  -i2,4 
-
-#	Dibujar Ejes de datos
-	gmt basemap -R0/$KM/$Topo -Byag+l"Elevaciones (m)"              -BW --FONT_ANNOT_PRIMARY=8,Helvetica,blue
-	gmt basemap -R0/$KM/$Grav -Bya+l"Anomal\355a Aire Libre (mGal)" -BE --FONT_ANNOT_PRIMARY=8,Helvetica,red
 
 #	Coordenadas Perfil (E, O)
 	echo O | gmt text -F+cTL+f14p -Gwhite -W1
 	echo E | gmt text -F+cTR+f14p -Gwhite -W1
 
+#	Ubicar leyenda
+	gmt legend -DjTR+o0.5c -F+gwhite+pthicker # --FONT_ANNOT_PRIMARY=14p,Helvetica-Bold
+
 #   ----------------------------------------------------------------------------------
 #	Cerrar la sesion y mostrar archivo
 gmt end
 
-	rm tmp_*
+#	Borrar achivos temporales
+	rm tmp_* -f
 
 #	Ejercicios sugeridos
-#	1. 
+#	1. Cambiar la segunda grilla y ajusta
