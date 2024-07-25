2641bae819ab8cd3dd69c61ce07344341a6738ed
Esteban82 <federico.esteban@gmail.com>
2024-07-25 00:40:32 -0300

    3

--- 1_Ejercicios/36_Pendiente+Aspecto/36_Pendiente_Aspecto.sh
+++ 1_Ejercicios/36_Pendiente+Aspecto/36_Pendiente_Aspecto.sh
@@ -7,25 +7,27 @@
 #	Definir variables del mapa
 #	-----------------------------------------------------------------------------------------------------------
 #	Titulo del mapa
-	title=42_Pendiente_Aspecto
+	title=36_Pendiente_Aspecto
+	title=$(basename $0 .sh)
 	echo $title
 
 #	Proyeccion y Region
 	PROJ=M15c
 	REGION=-72/-64/-35/-30
+#	REGION=AR
 #	REGION=-68.5/-67.5/-32/-31
 
-	
 #	Grilla	
-	DEM=@earth_relief_30s_p
+	DEM=@earth_relief_30s
 
 #	CPT creadas ad-hoc
 #	CPT=Aspect-Slope_Brewer
 	CPT=Aspect-Slope_Esteban
 
 # 	Nombre archivo de salida
-	CUT=tmp_$title.nc
-	CUT2=tmp_$title-2.nc
+	PENDIENTE=tmp_pendiente.nc
+	ASPECTO=tmp_aspecto.nc
+	PEND_ASPECTO=Pendiente_Aspecto.nc
 
 #	Dibujar mapa
 #	-----------------------------------------------------------------------------------------------------------
@@ -36,30 +38,30 @@ gmt begin $title png
 	gmt basemap -R$REGION -J$PROJ -B+n
 
 #	Calcular Grillas con modulo del pendiente (-D -S) y aspecto (-Da)
-	gmt grdgradient $DEM -R$REGION -fg -D  -S$CUT
-	gmt grdgradient $DEM -R$REGION -fg -Da -G$CUT2
+	gmt grdgradient $DEM -R$REGION -fg -D  -S$PENDIENTE
+	gmt grdgradient $DEM -R$REGION -fg -Da -G$ASPECTO
 
 #	Convertir modulo del gradiente a inclinacion (pendiente) en grados (ATAND).
-	gmt grdmath $CUT ATAND = $CUT
+	gmt grdmath $PENDIENTE ATAND = $PENDIENTE
 
 #	Reclasificar grilla de pendiente en 4 clases.
 # 	Pendiente muy baja (menor a A), baja (entre A y B), moderada (entre B y C) y alta (mayor a C).
 	A=3 ; B=12 ; C=22	# Valores originales definidos por Bewer (1993)	
 #	A=1 ; B=10 ; C=20
 #	A=1 ; B=05 ; C=10
-	gmt grdclip $CUT -G$CUT -Sb$A/10 -Si$A/$B/20 -Si$B/$C/30 -Sa$C/40
+	gmt grdclip $PENDIENTE -G$PENDIENTE -Sb$A/10 -Si$A/$B/20 -Si$B/$C/30 -Sa$C/40
 
 #	Reclasificar grilla de Aspecto en 8 clases 
 #	En rangos de 45º en cada direccion (N, NE, E, SE, S, SO, O, NO)
-	gmt grdclip $CUT2 -G$CUT2 -Sb22.5/1 -Si22.5/67.5/2 -Si67.5/112.5/3 -Si112.5/157.5/4 -Si157.5/202.5/5 \
+	gmt grdclip $ASPECTO -G$ASPECTO -Sb22.5/1 -Si22.5/67.5/2 -Si67.5/112.5/3 -Si112.5/157.5/4 -Si157.5/202.5/5 \
 	-Si202.5/247.5/6 -Si247.5/292.5/7 -Si292.5/337.5/8 -Sa337.5/1
 
 #	-------------------------------------------------------------------------------
 #	Sumar Grillas
-	gmt grdmath $CUT $CUT2 ADD = $CUT
+	gmt grdmath $PENDIENTE $ASPECTO ADD = $PEND_ASPECTO
 
 #	Crear Imagen
-	gmt grdimage $CUT -C$CPT.cpt
+	gmt grdimage $PEND_ASPECTO -C$CPT.cpt
 
 #	-----------------------------------------------------------------------------------------------------------
 #	Dibujar frame
