c26b797c4aeab182f09cf0b4ee50236433cda147
Esteban82 <federico.esteban@gmail.com>
2024-07-28 12:40:28 -0300

    sed version final

--- 1_Ejercicios/x/42_sed/42_sed.sh
+++ 1_Ejercicios/x/42_sed/42_sed.sh
@@ -1,62 +1,124 @@
+# Temas a ver. Ejemplos basicos con SED (Stream Editor)
+# SED (stream editor) es una herramienta para buscar, reemplazar, insertar y borrar lineas de texto.
+
 # Ejemplos de SED
 ARCHIVO=ejemplo.txt
 
+# 0. Ejemplo de ARCHIVO
 cat > $ARCHIVO <<-   EOF
-1. hello
-2. world
-3. This is line three.
-4. Here is the final line.
-5. Nueva linea
+1. hola
+2. mundo
+3. Esta es la linea tres.
+4. Esta es la ultima linea.
+5. Nueva linea.
 EOF
 
-echo Archivo original:
-cat $ARCHIVO
+# 1. Ver contenido original en terminal
+# cat $ARCHIVO
 
-# 1. Imprimir lineas (p)
-#echo '1p' -n Imprimir primera linea 
-#sed -n '1p' example.txt
+# 2. Imprimir lineas (p)
+# -----------------------------------------------------------------------------
+# A. Imprimir primera linea y resto del archivo 
+#sed '1p' $ARCHIVO
 
-echo -n '$p' Imprimir ultima linea:
-sed -n '$p' $ARCHIVO
+# A. Solo imprimir primera linea 
+# -n: suprime imprimir automaticamente el archivo.
+#sed -n '1p' $ARCHIVO
 
-echo -n '1,4p' Imprimir 4 primeras lineas:
-sed -n '1,4p' $ARCHIVO
+# B. Imprimir ultima linea
+#sed -n '$p' $ARCHIVO
+# En todos los ejemplos, el resultado se puede guardar en un archivo con >
+#sed -n '$p' $ARCHIVO > nuevo_archivo.txt
 
-echo -n '1~2p' Imprime 1 linea de cada 2.
-sed -n '1~2p' $ARCHIVO
+# C. Imprimir 4 primeras lineas:
+#sed -n '1,4p' $ARCHIVO
 
-echo -n '2,+2p' Imprime linea 2, junto con las 2 siguientes
-sed -n '2,+2p' $ARCHIVO
+# D. Imprimir 1 de cada 2 lineas:
+#sed -n '1~2p' $ARCHIVO
+
+# E. Imprimir la linea 2, junto con las 2 siguientes:
+#sed -n '2,+2p' $ARCHIVO
 
 # 2. Borrar lineas
-#echo '1d' Borra la primera linea 
+# -----------------------------------------------------------------------------
+# A. Borrar la primera linea
 #sed '1d' $ARCHIVO
 
-#echo '2,3d' Borra las lineas 2 y 3
-#sed '2,3d'$ARCHIVO
+# B. Borrar las lineas 2 y 3
+#sed '2,3d' $ARCHIVO
 
-#echo '1!d' Borra todo excepto la primera linea (o sea solo deja la primera)
+# C. Borrar todo excepto la primera linea (o sea solo deja la primera)
 #sed '1!d' $ARCHIVO
 
-# -i: trabaja en el mismo archivo de entrada. 
-
 # 3. Buscar y reemplazar patrones
-#sed -i 's/hello/hola/g' $ARCHIVO
-#echo sed 's/hello/hola/g'
-#sed 's/hello/hola/g' $ARCHIVO
-echo sed 's/is/es/'
-sed 's/is/es/' $ARCHIVO
+# -----------------------------------------------------------------------------
+# A. Reemplaza "es" por "is" la PRIMERA VEZ que aparece en CADA LINEA
+#sed 's/es/is/' $ARCHIVO
 
-echo sed 's/is/es/g'
-sed 's/is/es/g' $ARCHIVO
+# 4. Ejemplos practicos
+# -----------------------------------------------------------------------------
+# A. Reemplazar comas por puntos
 
-rm $ARCHIVO
+# Crear ejemplo
+cat > coordenadas.txt <<- END
+-70,65 -33,44 Santiago
+-68,54 -31,54 San Juan
+END
 
-echo Reemplazar puntos por comas sed 's/./,/g'
-sed 's/./,/g' example.txt 
-echo Da mal. El "." es un comodin que significa cualquier caracter. Por eso reemplaza todas las letras, incluidos los espacios por ,.
-sed 's/\./,/g' example.txt
+# A. Reemplazar comas por puntos.
+#sed 's/,/./' coordenadas.txt  # Reemplaza solo la primera aparación en cada linea.
 
+# B. Reemplazar TODAS las comas por puntos
+# g: reemplazo global (o sea TODAS las veces que aparece en la linea)
+#sed 's/,/./g' coordenadas.txt 
 
+# C. Reemplazar en el mismo arhivo de entrada (-i)
+# -i: trabaja en el mismo archivo de entrada. 
+#sed -i 's/,/./g' coordenadas.txt 
 
-#sed -i "s/^ *//;s/ *$//;s/ \{1,\}/ /g" *.txt
+# C. Reemplazar puntos por comas
+#echo 3.14 1.618 | sed 's/./,/g'
+
+# Da mal. El "." es un comodin que significa cualquier caracter. 
+# Por eso reemplaza todo (letras, numeros, simbolos, espacios) por comas.
+# Hay que usar una \ para NO usar el comodín
+#echo 3.14 1.618 | sed 's/\./,/g'
+
+# D. Reemplaza las , por ;
+#sed 's/,/;/g' query.csv
+
+# D. Reemplaza las , por tabulaciones:
+#sed 's/,/\t/g' query.csv
+
+# B. Reemplaza una o más instancias de espacios en blanco consecutivos dentro de una línea por un solo espacio.
+
+# Crear ejemplo
+echo "Esta es una     línea con espacios.    ">  espacios.txt
+echo "    Otra  línea   con   más espacios. ">> espacios.txt
+
+# 
+#sed "s/ \{1,\}/ /g" espacios.txt
+
+cat espacios.txt
+# C. Elimina los espacios en blanco al inicio de cada línea.
+#sed 's/^ *//' espacios.txt
+
+# D. Elimina los espacios en blanco al final de cada línea.
+#sed 's/ *$//' espacios.txt
+
+# E. Reemplaza múltiples espacios en blanco consecutivos con un solo espacio.
+#sed 's/ \{1,\}/ /g' espacios.txt
+
+# F. Combina las 3 opciones anteriores
+sed 's/^ *//;s/ *$//;s/ \{1,\}/ /g' espacios.txt > espacios2.txt
+
+# Borrar arhivos
+ rm -f coordenadas.txt $ARCHIVO espacios*.txt
+
+# Comodines:
+# ^ = Inicio de una linea
+# $ = coincide con el final de linea
+# * = coincide con 0 o mas ocurrencias del caractér previo.
+# + = coincide con 1 o mas currencias del caractér previo.
+# ? = coincide con 0 o 1 ocurrencia del caractér previo.
+# . = coincide exactamente con un caractér.
